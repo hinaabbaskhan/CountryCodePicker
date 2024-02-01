7cd418fcde6b9e547cfa86317499d581a284eb29
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-02-01 13:44:59 +0500

    update space between widgets

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -127,7 +127,8 @@ class CountryCodePicker extends StatefulWidget {
     this.dialogBackgroundColor,
     this.closeIcon = const Icon(Icons.close),
     this.countryList = codes,
-    this.dialogItemPadding = const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+    this.dialogItemPadding =
+        const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
     this.searchPadding = const EdgeInsets.symmetric(horizontal: 24),
     Key? key,
   }) : super(key: key);
@@ -195,8 +196,8 @@ class CountryCodePickerState extends State<CountryCodePicker> {
                         : Clip.hardEdge,
                     decoration: widget.flagDecoration,
                     margin: widget.alignLeft
-                        ? const EdgeInsets.only(right: 16.0, left: 8.0)
-                        : const EdgeInsets.only(right: 16.0),
+                        ? const EdgeInsets.only(right: 8.0, left: 8.0)
+                        : const EdgeInsets.only(right: 8.0),
                     child: Image.asset(
                       selectedItem!.flagUri!,
                       package: 'country_code_picker',
@@ -222,8 +223,8 @@ class CountryCodePickerState extends State<CountryCodePicker> {
                   fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
                   child: Padding(
                       padding: widget.alignLeft
-                          ? const EdgeInsets.only(right: 16.0, left: 8.0)
-                          : const EdgeInsets.only(right: 16.0),
+                          ? const EdgeInsets.only(right: 8.0, left: 8.0)
+                          : const EdgeInsets.only(right: 8.0),
                       child: Icon(
                         Icons.arrow_drop_down,
                         color: Colors.grey,
